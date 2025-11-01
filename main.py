a96ba338a76da06008964f49f67ac4dca4501e8d
venesa93 <venesabolf93@gmail.com>
2025-11-01 16:49:23 -0500

    Final Commit Project 7.

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ class TestUrbanRoutes:
     @classmethod
     def setup_class(cls):
         if helpers.is_url_reachable(data.URBAN_ROUTES_URL):
-          print ("Connected to the Urban Routes server")
+            print ("Connected to the Urban Routes server")
         else:
-          print ("Cannot connect to Urban Routes. Check the server is on and still running")
+            print ("Cannot connect to Urban Routes. Check the server is on and still running")
 
     def test_set_route(self):
         #Add in S8
